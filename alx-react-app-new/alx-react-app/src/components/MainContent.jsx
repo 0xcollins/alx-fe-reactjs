dd5d0ad41c99aef2cbfd2b5ee4107b656425f418
“Collins <61962307+0xcollins@users.noreply.github.com>
2024-08-19 04:26:33 +0530

    updates

--- alx-react-app-new/alx-react-app/src/components/MainContent.jsx
+++ alx-react-app-new/alx-react-app/src/components/MainContent.jsx
@@ -1,9 +1,11 @@
 function MainContent() {
     return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
+      <main>
+        <p style={{ color: "skyblue", fontSize: "18px", marginBottom: "50px" }}>
+          I love to visit New York, Paris, and Tokyo.
+        </p>
+      </main>
     );
-}
-
-export default MainContent;
+  }
+  
+  export default MainContent;
