d37bed0e716ac7eac22c3994140b76601b91dde5
“Collins <61962307+0xcollins@users.noreply.github.com>
2024-08-24 06:27:30 +0530

    update

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
-const UserProfile = (props) => {
-    return (
-      <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
-        <h2 style={{ color: "blue" }}>{props.name}</h2>
-        <p>
-          Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
-        </p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  
-  export default UserProfile;
+import React, { useContext } from 'react';
+import UserContext from './UserContext'; // Adjust the path based on where UserContext.js is located
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
