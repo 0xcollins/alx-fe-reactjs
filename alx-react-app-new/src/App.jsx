d8ae3945ffaec2ea16793b896fcff63312ecf3a5
“Collins <61962307+0xcollins@users.noreply.github.com>
2024-08-25 03:01:57 +0530

    updates

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,12 +7,18 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+      <h1 style={{ textAlign: 'center' }}>Simple Counter Application</h1>
+      <Counter />
+      </div>
       <div>
          {/* Other components like Header, MainContent, etc. */}
          <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
