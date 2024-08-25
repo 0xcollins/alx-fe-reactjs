3f618a28d864fd2ced0a46a034980b28a800cca9
“Collins <61962307+0xcollins@users.noreply.github.com>
2024-08-26 04:11:05 +0530

    update

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import useRecipeStore from '../store/recipeStore'
+// src/components/RecipeList.jsx
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes)
+  const recipes = useRecipeStore(state => state.filteredRecipes); // Use filteredRecipes
 
   return (
     <div>
-      <h2>Recipes</h2>
-      {recipes.map((recipe) => (
+      {recipes.map(recipe => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+          <Link to={`/recipes/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default RecipeList
+export default RecipeList;
