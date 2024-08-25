3f68cd8ba28cd8f1196b45238b768e6c8f2b819a
“Collins <61962307+0xcollins@users.noreply.github.com>
2024-08-26 03:15:03 +0530

    update

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,8 +1,19 @@
 import { create } from 'zustand';
 
+// Create the Zustand store
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
   setRecipes: (recipes) => set({ recipes }),
 }));
 
