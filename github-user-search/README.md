7c3fb9e5fd4e5c503ae90f59c1eb9c01ab5ca941
Collins Akpaka <61962307+0xcollins@users.noreply.github.com>
2024-09-30 03:46:21 +0530

    Delete github-user-search directory

--- github-user-search/README.md
+++
@@ -1,3 +0,0 @@
-# GitHub User Search Application
-
-This application allows users to search for GitHub profiles using the GitHub API.
--- github-user-search/github-user-search/src/services/githubService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const fetchAdvancedUserData = async (query) => {
-    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
-    return response.data.items;
-  };
-  
